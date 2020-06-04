9ca81093ac3f1aaaa366e3eba15b80eaf1893207
Rupali Rajput <rupalirajput3062000@gmail.com>
2020-06-04 23:08:06 +0530

    Error Hansling

--- app.js
+++ app.js
@@ -7,5 +7,21 @@ const ordersRoute = require('./api/Rotes/orders')
 app.use("/products" , productsRoute);
 app.use("/orders" , ordersRoute);
 
+app.use((req,res,next)=>{
+    const error = new Error("Not found")
+    res.status(404)
+    next(error)
+});
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500)
+    res.json({
+        error:{
+            message:error.message
+        }
+    })
+
+});
+
+
 
 module.exports = app
